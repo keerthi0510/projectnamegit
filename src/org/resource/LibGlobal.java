7e8ed21b8f251ef2215520b61497e033801ec0f2
keerthi0510 <swakeerthi510@gmail.com>
2021-09-08 09:25:54 +0530

    username functionality

--- src/org/resource/LibGlobal.java
+++ src/org/resource/LibGlobal.java
@@ -2,4 +2,12 @@ package org.resource;
 
 public class LibGlobal {
 
+	public static void password() {
+		System.out.println("password");
+	}
+	
+	public static void main(String[] args) {
+		password();
+}
+
 }
